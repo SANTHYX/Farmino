8c1a5df01fd35452d3ea4479ea673da25ef8eb03
Damian Dorocicz <damiand44444@gmail.com>
2020-10-09 21:27:42 +0200

    Added Response Body

---
+++ Inżynierka/nazwa_inżynierki/Backend/Farmino.Service/Service/ServiceResponse/ServiceResponse.cs
@@ -0,0 +1,11 @@
+﻿using Farmino.Service.Service.Interfaces;
+
+namespace Farmino.Service.Service.ServiceResponse
+{
+    public class ServiceResponse<T> where T : IService
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+}
