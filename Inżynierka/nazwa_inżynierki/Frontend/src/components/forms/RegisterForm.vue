cff253548c246709c8f79ef0cdf175ccc708e730
Damian Dorocicz <damiand44444@gmail.com>
2020-10-13 19:44:43 +0200

    Create Register Form

--- Inżynierka/nazwa_inżynierki/Frontend/src/components/forms/RegisterForm.vue
+++ Inżynierka/nazwa_inżynierki/Frontend/src/components/forms/RegisterForm.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="register-form">
     <h1>Rejestracja</h1>
-    <div class="form" id="account-form"></div>
+    <div class="form" id="account-form">
+      <h2>Dane Logowania</h2>
+      <label for="login-form">Login</label>
+      <input type="text" name="login-form" id="login-form" />
+      <label for="login-form">Login</label>
+      <input type="text" name="login-form" id="login-form" />
+      <label for="login-form">Login</label>
+      <input type="text" name="login-form" id="login-form" />
+    </div>
     <div class="form" id="personal-data-form"></div>
     <div class="form" id="address-form"></div>
   </div>
@@ -13,4 +21,17 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#register-form {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  #account-form {
+    display: flex;
+    flex-direction: column;
+    justify-self: center;
+    align-self: center;
+  }
+}
+</style>
