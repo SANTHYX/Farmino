be7b77d3a3c42fd01343ccf4d1dba68ddd4199bd
Damian Dorocicz <damiand44444@gmail.com>
2020-12-28 21:51:10 +0100

    Changes

--- Inżynierka/Farmino/Backend/Farmino.Service/Service/Interfaces/IProductService.cs
+++ Inżynierka/Farmino/Backend/Farmino.Service/Service/Interfaces/IProductService.cs
@@ -1,6 +1,14 @@
-﻿namespace Farmino.Service.Service.Interfaces
+﻿using Farmino.Data.Models.Entities;
+using Farmino.Service.DTO;
+using System;
+using System.Threading.Tasks;
+
+namespace Farmino.Service.Service.Interfaces
 {
     public interface IProductService : IService
     {
+        Task AddAsync(string name, decimal price, int quantity,
+            int unit, double value);
+        Task<ProductDTO> GetAsync(Guid id);
     }
 }
--- Inżynierka/Farmino/Backend/Farmino.Service/Service/ProductService.cs
+++ Inżynierka/Farmino/Backend/Farmino.Service/Service/ProductService.cs
@@ -1,9 +1,21 @@
-﻿using Farmino.Service.Service.Interfaces;
+﻿using Farmino.Service.DTO;
+using Farmino.Service.Service.Interfaces;
+using System;
+using System.Threading.Tasks;
 
 namespace Farmino.Service.Service
 {
     public class ProductService : IProductService
     {
 
+        public Task AddAsync(string name, decimal price, int quantity, int unit, double value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductDTO> GetAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
