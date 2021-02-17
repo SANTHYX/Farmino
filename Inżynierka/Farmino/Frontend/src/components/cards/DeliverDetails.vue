00f0a47c0d664cd197a0763e8e62e1f015725d5b
Damian Dorocicz <damiand44444@gmail.com>
2021-02-17 20:47:30 +0100

    changes

--- Inżynierka/Farmino/Frontend/src/components/cards/DeliverDetails.vue
+++ Inżynierka/Farmino/Frontend/src/components/cards/DeliverDetails.vue
@@ -3,6 +3,19 @@
     <div id="deliver-details">
       <h1>Szczegóły</h1>
       <hr />
+
+      <div>
+        <h2></h2>
+      </div>
+
+      <div>
+        <h2>Adres Klienta</h2>
+        <p>{{ `${order.orderDetails.firstName} ${order.orderDetails.lastName}` }}</p>
+        <p>
+          {{ `${order.orderDetails.address.street} ${order.orderDetails.address.houseNumber}` }}
+        </p>
+        <p>{{ `${order.orderDetails.address.city} ${order.orderDetails.address.postalCode}` }}</p>
+      </div>
       <button @click="checkDeliveryOrdersList">Wróć</button>
     </div>
   </div>
@@ -41,13 +54,25 @@ export default {
 
 <style lang="scss" scoped>
 #deliver-details {
+  margin: 12rem 0;
   display: flex;
   flex-direction: column;
-  justify-content: space-evenly;
-  justify-items: center;
-  border-top: 1px solid rgb(211, 211, 211);
-  border-bottom: 1px solid rgb(211, 211, 211);
+  align-items: center;
+  flex-wrap: wrap;
+  border: 1px solid rgb(216, 216, 216);
   min-width: 20vw;
-  padding: 0.5rem;
+  min-height: 70vh;
+  box-shadow: 1px 1px 6px rgba(179, 179, 179, 0.746);
+
+  h1 {
+    margin: 0.75rem 0 0 0;
+    color: rgb(51, 51, 51);
+  }
+
+  hr {
+    width: 200px;
+    color: rgb(228, 228, 228);
+    margin: 0 0 1rem 0;
+  }
 }
 </style>
